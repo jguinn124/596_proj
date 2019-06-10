c1efcc74e758c6951734881093fd5155e6c7fb60
John Guinn <jguinn124@gmail.com>
2019-06-10 16:41:06 -0500

    Name changes

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send(index.html));
+app.get('/', (req, res) => res.send(main.html));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
