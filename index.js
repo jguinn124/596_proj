305cd0caea6db9b379ad5e88c2842b889cea1834
John Guinn <jguinn124@gmail.com>
2019-06-10 16:47:18 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-'use strict'
+'use strict';
 
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send(main.html));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/main.html')));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
