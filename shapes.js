8b860fd9677c1162648a022d3667f41be90c607c
John Guinn <jguinn124@gmail.com>
2019-06-11 17:27:11 -0500

    Update shapes.js

--- shapes.js
+++ shapes.js
@@ -14,9 +14,40 @@ svg.selectAll("rect")
 		.attr("x", function(d,i){return 60*i+30;})
 		.attr("y", function(d,i){return 300-(d*15);});
 
-svg.selectAll("circle")
+let newX = 300;
+svg.selectAll("circle.first")
 	.data(dataArray)
 	.enter().append("circle")
-		.attr("cx", function(d,i){return 300+(i*20);})
+		.attr("class", "first")
+		.attr("cx", function(d,i){
+			newX+=(d*3)+(i*20);
+			return newX;
+		})
 		.attr("cy", "100")
 		.attr("r", function(d,i){return d*3});
+
+
+newX = 600;
+svg.selectAll("ellipse")
+.data(dataArray)
+.enter().append("ellipse")
+	.attr("cx", function(d,i){
+		newX+=(d*3)+(i*20);
+		return newX;
+	})
+	.attr("cy", "100")
+	.attr("rx", function(d,i){return d*3})
+	.attr("ry", "30");
+
+
+newX = 900;
+svg.selectAll("line")
+.data(dataArray)
+.enter().append("line")
+	.attr("cx", function(d,i){
+		newX+=(d*3)+(i*20);
+		return newX;
+	})
+	.attr("cy", "100")
+	.attr("rx", function(d,i){return d*3})
+	.attr("ry", "30");
