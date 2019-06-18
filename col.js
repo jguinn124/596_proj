08b87b686ed95c38e923152e83765188986ad575
John Guinn <jguinn124@gmail.com>
2019-06-18 15:02:12 -0500

    Update col.js

--- col.js
+++ col.js
@@ -9,4 +9,16 @@ d3.csv("Col.csv").get(function(error, data){
 	let max = d3.max(data, function(d){return d.COV; });
 	console.log(max);
 
+
+	let maxYear = d3.max(data, function(d){return d.Year; });
+	let minYear = d3.min(data, function(d){return d.Year; });
+
+	let y = d3.scaleLinear()
+					.domain(0,max)
+					.range(height,0);
+	
+	let x = d3.scaleTime()
+					.domain(minYear,maxYear)
+					.range(0,width);
+
 });
