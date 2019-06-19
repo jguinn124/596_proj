83892f8492dab47d4d558dd0c6e0289e3ca0061e
John Guinn <jguinn124@gmail.com>
2019-06-19 15:41:11 -0500

    Update col.js

--- col.js
+++ col.js
@@ -1,7 +1,9 @@
 'use strict';
 
-d3.csv("Col.csv").get(function(error, data){
-	//console.log(data);
+d3.csv('Col.csv')
+	.row(function(d){ return {month: d.Year, COL:Number(d.COL)}; })
+	.get(function(error, data){
+	console.log(data);
 
 	let height = 300;
 	let width = 500;
@@ -14,28 +16,28 @@ d3.csv("Col.csv").get(function(error, data){
 	let minYear = d3.min(data, function(d){return d.Year; });
 
 	let y = d3.scaleLinear()
-					.domain(0,max)
-					.range(height,0);
+		.domain(0,max)
+		.range(height,0);
 	
 	let x = d3.scaleTime()
-					.domain(minYear,maxYear)
-					.range(0,width);
+		.domain(minYear,maxYear)
+		.range(0,width);
 
 	let yAxis = d3.axisLeft(y);
 	let xAxis = d3.axisBottom(x);
 
-	let svg = d3.select("body").append("svg")
-					.attr("height", "100%")
-					.attr("width", "100%");
+	let svg = d3.select('body').append('svg')
+		.attr('height', '100%')
+		.attr('width', '100%');
 
 	let margin = {left:50, right:50, top:40, bottom:0};
 
-	let chartGroup = svg.append("g")
-					.attr("transform", "translate("+margin.left+","+margin.right+")");
+	let chartGroup = svg.append('g')
+		.attr('transform', 'translate('+margin.left+','+margin.right+')');
 
 	let line = d3.line()
-					.x(function(d){ return x(d.Year); })
-					.y(function(d){ return y(d.COL); });
+		.x(function(d){ return x(d.Year); })
+		.y(function(d){ return y(d.COL); });
 
-	chartGroup.append("path").attr("d", line(data));
+	chartGroup.append('path').attr('d',line(data));
 });
